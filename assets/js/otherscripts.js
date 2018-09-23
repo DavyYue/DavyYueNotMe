834e8741b32204b3383dd67085f954ca7065377a
ddyy345 <davy.yue@vanderbilt.edu>
2018-09-23 13:13:23 -0500

    Add tabs js

--- assets/js/otherscripts.js
+++ assets/js/otherscripts.js
@@ -16,3 +16,18 @@ openButton.addEventListener("click", function() {
 modal.classList.toggle("closed");
 modalOverlay.classList.toggle("closed");
 });
+
+//Tabs
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(cityName).style.display = "block";
+    if (evt) {evt.currentTarget.classList.add("active");}
+}
